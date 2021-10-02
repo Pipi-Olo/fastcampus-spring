ba591732c2e99e608f5e979122bdfa415f5006d9
Pipi-Olo <Copy.H13@gmail.com>
2021-10-03 02:23:36 +0900

    Add #1 - Dtos
    Add Dto Class for Reading AllDevelopers and a DetailDeveloper

---
+++ Part 1. Spring Framework/dmaker/src/main/java/com/pipiolo/dmaker/dto/DeveloperDetailDto.java
@@ -0,0 +1,35 @@
+package com.pipiolo.dmaker.dto;
+
+import com.pipiolo.dmaker.entity.Developer;
+import com.pipiolo.dmaker.type.DeveloperLevel;
+import com.pipiolo.dmaker.type.DeveloperSkillType;
+import com.pipiolo.dmaker.code.StatusCode;
+import lombok.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class DeveloperDetailDto {
+
+    private DeveloperLevel developerLevel;
+    private DeveloperSkillType developerSkillType;
+    private Integer experienceYears;
+    private String memberId;
+    private StatusCode statusCode;
+    private String name;
+    private Integer age;
+
+    public static DeveloperDetailDto fromEntity(Developer developer) {
+        return DeveloperDetailDto.builder()
+                .developerLevel(developer.getDeveloperLevel())
+                .developerSkillType(developer.getDeveloperSkillType())
+                .experienceYears(developer.getExperienceYears())
+                .memberId(developer.getMemberId())
+                .statusCode(developer.getStatusCode())
+                .name(developer.getName())
+                .age(developer.getAge())
+                .build();
+    }
+}
