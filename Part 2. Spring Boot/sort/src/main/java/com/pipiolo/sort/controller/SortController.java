fd7e68f98d2a976bd1b7d58f038536fe23e6183a
Pipi-Olo <Copy.H13@gmail.com>
2021-10-04 06:51:08 +0900

    Add #4 - Lombok
    Lombok 의존성 추가 및 Update SortController.java

--- Part 2. Spring Boot/sort/src/main/java/com/pipiolo/sort/controller/SortController.java
+++ Part 2. Spring Boot/sort/src/main/java/com/pipiolo/sort/controller/SortController.java
@@ -1,20 +1,18 @@
 package com.pipiolo.sort.controller;
 
 import com.pipiolo.sort.service.SortService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @RestController
 public class SortController {
 
     private final SortService sortService;
-
-    public SortController(SortService sortService) {
-        this.sortService = sortService;
-    }
 
     @GetMapping("/")
     public String doSort(@RequestParam List<String> list) {
