e6b186875541a2889eaf1e009eb2402dae80e403
Pipi-Olo <Copy.H13@gmail.com>
2021-10-06 18:38:42 +0900

    Initial #7 - Note Application
    Initialize Note Application with Spring-Security

---
+++ Part 5. Spring Security/security-note/src/main/java/com/pipiolo/note/SecurityNoteApplication.java
@@ -0,0 +1,13 @@
+package com.pipiolo.note;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityNoteApplication.class, args);
+    }
+
+}
---
+++ Part 5. Spring Security/security-note/src/test/java/com/pipiolo/note/SecurityNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipiolo.note;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
