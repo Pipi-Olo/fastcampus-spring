b9581e5fd99f07a0b8706603fad1c24261285b05
Pipi-Olo <Copy.H13@gmail.com>
2021-10-25 19:54:10 +0900

    Update #10 - EventRepository.java
    @Query 를 통해 쿼리문 직접 입력방식과
    쿼리메소드(메소드 이름이 곧 쿼리 표현) 확인

--- Part 4. Spring Data JPA/get-in-line/src/main/java/com/pipiolo/getinline/repository/EventRepository.java
+++ Part 4. Spring Data JPA/get-in-line/src/main/java/com/pipiolo/getinline/repository/EventRepository.java
@@ -1,18 +1,31 @@
 package com.pipiolo.getinline.repository;
 
+import com.google.common.eventbus.EventBus;
+import com.pipiolo.getinline.constant.EventStatus;
 import com.querydsl.core.types.dsl.ComparableExpression;
 import com.querydsl.core.types.dsl.StringExpression;
 import com.pipiolo.getinline.domain.Event;
 import com.pipiolo.getinline.domain.QEvent;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
 import org.springframework.data.querydsl.binding.QuerydslBindings;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface EventRepository extends
+        //EventReadOnlyRepository<Event, Long>,
         JpaRepository<Event, Long>,
         QuerydslPredicateExecutor<Event>,
         QuerydslBinderCustomizer<QEvent> {
+
+    // @Query("select e from Event e where eventName = :eventName and eventStatus = :eventStatus")
+    List<Event> findByEventNameAndEventStatus(String eventName, EventStatus eventStatus);
+
+    Optional<Event> findFirstByEventEndDatetimeBetween(LocalDateTime from, LocalDateTime to);
 
     @Override
     default void customize(QuerydslBindings bindings, QEvent root) {
