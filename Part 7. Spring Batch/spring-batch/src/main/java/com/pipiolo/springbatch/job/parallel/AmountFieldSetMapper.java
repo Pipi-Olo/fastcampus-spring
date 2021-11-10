413b1ed4efea009dd1944c45ce2f70343f5f2c83
Pipi-Olo <Copy.H13@gmail.com>
2021-11-11 02:02:11 +0900

    Add #12 - MultiThreadStepJobConfig
    MuitlThread 중 SimpleAsyncTaskExecutor(1 processor - multi thread) 사용한 job

--- Part 7. Spring Batch/spring-batch/src/main/java/com/pipiolo/springbatch/job/parallel/AmountFieldSetMapper.java
+++ Part 7. Spring Batch/spring-batch/src/main/java/com/pipiolo/springbatch/job/parallel/AmountFieldSetMapper.java
@@ -1,4 +1,19 @@
 package com.pipiolo.springbatch.job.parallel;
 
-public class AmountFieldSetMapper {
+import com.pipiolo.springbatch.dto.AmountDto;
+import org.springframework.batch.item.file.mapping.FieldSetMapper;
+import org.springframework.batch.item.file.transform.FieldSet;
+import org.springframework.validation.BindException;
+
+public class AmountFieldSetMapper implements FieldSetMapper<AmountDto> {
+
+    @Override
+    public AmountDto mapFieldSet(FieldSet fieldSet) throws BindException {
+        AmountDto amount = new AmountDto();
+        amount.setIndex(fieldSet.readInt(0));
+        amount.setName(fieldSet.readString(1));
+        amount.setAmount(fieldSet.readInt(2));
+
+        return amount;
+    }
 }
