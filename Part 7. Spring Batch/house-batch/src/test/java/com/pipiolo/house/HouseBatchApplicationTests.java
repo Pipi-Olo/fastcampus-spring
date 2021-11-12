5297f133e9b83b75b533b02050ea5f11f9aa13bd
Pipi-Olo <Copy.H13@gmail.com>
2021-11-12 10:08:04 +0900

    Initialize #12 - HouseBatchApplicaion
    Hogang-nono Application Using Data.go.kr

---
+++ Part 7. Spring Batch/house-batch/src/test/java/com/pipiolo/house/HouseBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipiolo.house;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HouseBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
