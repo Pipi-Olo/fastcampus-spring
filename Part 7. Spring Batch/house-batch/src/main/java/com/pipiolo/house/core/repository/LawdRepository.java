03325ed3af1dd6e68a5d1c8b8444d433c9c7be9b
Pipi-Olo <Copy.H13@gmail.com>
2021-11-14 23:38:17 +0900

    Add#12 - Lawd Domain and Repository
    Add Lawd(법정동) Domain and Repository
    
    Data.go.kr 의 분류코드에 따른 '동'을 저장하는 도메인 및 저장소

---
+++ Part 7. Spring Batch/house-batch/src/main/java/com/pipiolo/house/core/repository/LawdRepository.java
@@ -0,0 +1,7 @@
+package com.pipiolo.house.core.repository;
+
+import com.pipiolo.house.core.entity.Lawd;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LawdRepository extends JpaRepository<Lawd, Long> {
+}
