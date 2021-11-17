c610951d2c1515fabf21a8f06eed3a4345c5b559
Pipi-Olo <Copy.H13@gmail.com>
2021-11-18 01:03:53 +0900

    Update #12 - AptDeal
    AptDealService : AptDto(결과 출력 dto) 로 변환해주는 서비스 로직 추가

--- Part 7. Spring Batch/house-batch/src/main/java/com/pipiolo/house/core/service/AptDealService.java
+++ Part 7. Spring Batch/house-batch/src/main/java/com/pipiolo/house/core/service/AptDealService.java
@@ -1,6 +1,7 @@
 package com.pipiolo.house.core.service;
 
 import com.pipiolo.house.core.dto.AptDealDto;
+import com.pipiolo.house.core.dto.AptDto;
 import com.pipiolo.house.core.entity.Apt;
 import com.pipiolo.house.core.entity.AptDeal;
 import com.pipiolo.house.core.repository.AptDealRepository;
@@ -8,6 +9,10 @@ import com.pipiolo.house.core.repository.AptRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,5 +42,13 @@ public class AptDealService {
         aptDeal.setDealCanceledDate(dto.getDealCanceledDate());
 
         aptDealRepository.save(aptDeal);
+    }
+
+    public List<AptDto> findByGuLawdCdAndDealDate(String guLawdCd, LocalDate dealDate) {
+        return aptDealRepository.findByDealCanceledIsFalseAndDealDateEquals(dealDate)
+                .stream()
+                .filter(aptDeal -> aptDeal.getApt().getGuLawdCd().equals(guLawdCd))
+                .map(aptDeal -> new AptDto(aptDeal.getApt().getAptName(), aptDeal.getDealAmount()))
+                .collect(Collectors.toList());
     }
 }
