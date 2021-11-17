5510e959d61d37ed00f96895c7c22b5c4c0f92b8
Pipi-Olo <Copy.H13@gmail.com>
2021-11-18 01:08:14 +0900

    Update #12 - AptDealRespository
    기존에는 N+1문제가 발생하여 조인으로 해결

--- Part 7. Spring Batch/house-batch/src/main/java/com/pipiolo/house/core/repository/AptDealRepository.java
+++ Part 7. Spring Batch/house-batch/src/main/java/com/pipiolo/house/core/repository/AptDealRepository.java
@@ -3,6 +3,7 @@ package com.pipiolo.house.core.repository;
 import com.pipiolo.house.core.entity.Apt;
 import com.pipiolo.house.core.entity.AptDeal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -13,5 +14,7 @@ public interface AptDealRepository extends JpaRepository<AptDeal, Long> {
     Optional<AptDeal> findAptDealByAptAndExclusiveAreaAndDealDateAndDealAmountAndFloor(
             Apt apt, Double exclusiveArea, LocalDate dealDate, Long dealAmount, Integer floor);
 
+    /* N+1 문제 발생 해결 */
+    @Query("select ad from AptDeal ad join fetch ad.apt where ad.dealCanceled = 0 and ad.dealDate = ?1")
     List<AptDeal> findByDealCanceledIsFalseAndDealDateEquals(LocalDate dealDate);
 }
