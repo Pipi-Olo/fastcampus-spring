741fbe5c96ee7297214cd3953c14b7ff16a5eb9e
Pipi-Olo <Copy.H13@gmail.com>
2021-11-20 22:37:00 +0900

    Add #15 - FakeAuthUser
    테스트할 때마다, 회원가입 후 로그인 테스트 할 수 없으니,
    자동으로 AuthUser를 만들어 주는 DevWevConfig생성
    
    이는 profile("dev") 에서만 작동함.
    
    이를통해 회원가입없이, 포스트맨으로 보내도 성공을 반환함

--- Part 10. Final Project/calendar/api/src/main/java/com/pipiolo/calendar/api/config/DevWebConfig.java
+++ Part 10. Final Project/calendar/api/src/main/java/com/pipiolo/calendar/api/config/DevWebConfig.java
@@ -1,21 +1,21 @@
 package com.pipiolo.calendar.api.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
-@Profile("!dev")
+@Profile("dev")
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class DevWebConfig implements WebMvcConfigurer {
 
     @Override
     public void addArgumentResolvers(
             List<HandlerMethodArgumentResolver> resolvers
     ) {
-        resolvers.add(new AuthUserResolver());
+        resolvers.add(new FakeAuthUserResolver());
     }
 }
