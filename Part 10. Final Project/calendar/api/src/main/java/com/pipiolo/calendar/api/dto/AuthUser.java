25445b5f08cc55b905ed18a78c647f29aa1e74b8
Pipi-Olo <Copy.H13@gmail.com>
2021-11-20 22:05:02 +0900

    Add #15 - ScheduleController
    Task 추가하는 메소드 구현.
    로그인 여부 확인 후, taskService 호출.

---
+++ Part 10. Final Project/calendar/api/src/main/java/com/pipiolo/calendar/api/dto/AuthUser.java
@@ -0,0 +1,17 @@
+package com.pipiolo.calendar.api.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AuthUser {
+
+    private Long id;
+
+    private AuthUser(Long id) {
+        this.id = id;
+    }
+
+    public static AuthUser of(Long id) {
+        return new AuthUser(id);
+    }
+}
